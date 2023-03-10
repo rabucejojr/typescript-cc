9a2cbb1037792477ef9303f3fb9dc709c0bab76b
Roger Abucejo <rogerabucejo62@gmail.com>
2023-03-10 13:12:38 +0800

    ts demo

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Demo from './components/Demo'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <>
+   <Demo/>
+   </>
+  )
+}
+
+export default App
